b09f98f7fcd591dfefeb7f4abdddcddea85d5cd1
DiegoL13 <diegolps1317@gmail.com>
2025-12-02 15:43:07 -0300

    App Funcionando

--- demo/src/main/java/com/example/StreamFlow/model/Filme.java
+++ demo/src/main/java/com/example/StreamFlow/model/Filme.java
@@ -1,5 +1,6 @@
 package com.example.StreamFlow.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,8 +10,12 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "filmes")
 public class Filme {
 
+    @Id // Marca o campo como a chave primária
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String diretor;
     private Integer duracaoMinutos;
--- demo/src/main/java/com/example/StreamFlow/model/Serie.java
+++ demo/src/main/java/com/example/StreamFlow/model/Serie.java
@@ -6,8 +6,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
